ded1364e069696f15bdc5dc2f6248280a99147e0
Rafael Soratto <rafael.soratto@appmoove.com.br>
2021-03-14 00:29:47 -0300

    ˜struture˜

---
+++ game/lib/pow.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow.shade700,
+      width: 60,
+      height: 60,
+      child: Image.asset(
+        'assets/images/roberto.png',
+      ),
+    );
+  }
+}
