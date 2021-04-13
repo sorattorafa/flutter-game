3898efcb8451ead1fc1d02491eb15d9bec999947
Rafael Soratto <rafael.soratto@appmoove.com.br>
2021-04-12 23:39:46 -0300

    make request

---
+++ pets/lib/servcices/pet/get.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+
+class OrdersHistoryService {
+  OrdersHistoryService(this._dio);
+
+  final Dio _dio;
+
+  Future<dynamic> listPetsByUser({
+    int page = 1,
+    int limit = 20,
+    List<String> status,
+    String name,
+  }) async {
+    final queryParameters = <String, dynamic>{
+      'page': page,
+      'limit': limit,
+      'order': jsonEncode([
+        {'updated_at': 'desc'}
+      ]),
+    };
+
+    try {
+      final response = await _dio.get<Map<String, dynamic>>(
+        'v1/orders',
+        queryParameters: queryParameters,
+      );
+    } on Object catch (error) {
+      print(error);
+      throw OrdersHistoryError.UNKNOWN_ERROR;
+    }
+  }
+
+  Future<dynamic> makeLogin(
+    String email,
+    String password,
+  ) async {
+    try {
+      //   return Model.fromJson(response.data);
+    } on Object catch (error) {
+      print(error);
+      throw OrdersHistoryError.UNKNOWN_ERROR;
+    }
+  }
+}
+
+enum OrdersHistoryError {
+  UNKNOWN_ERROR,
+}
