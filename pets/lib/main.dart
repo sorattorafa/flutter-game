58b9b85c6cda2c5a2fb155669d5646e54916c634
Rafael Rampim Soratto <rafael.soratto@appmoove.com.br>
2021-04-19 18:05:37 -0300

    remove flavor config

--- pets/lib/main.dart
+++ pets/lib/main.dart
@@ -1,11 +1,6 @@
-// @dart=2.6
-
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:pets/app.dart';
-import 'flavor-config.dart';
 void main() {
-  FlavorConfig(
-      flavor: Flavor.USER_DEV,
-      values: FlavorValues(baseUrl: 'http://127.0.0.1:5000/'));
   runApp(PetsApp());
 }
